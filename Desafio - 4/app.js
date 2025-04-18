b306f7eed979b770f4b42a80a787b7135c978e00
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-18 20:00:58 -0300

    Desafio 4
    Concluído

---
+++ Desafio - 4/app.js
@@ -0,0 +1,4 @@
+let linguagemDeProgramação = prompt(`Qual sua linguagem de programação que mais gosta?`)
+
+console.log(`${linguagemDeProgramação} é uma exelente linguagem`);
+alert(`${linguagemDeProgramação} é uma excelente escolha!`);

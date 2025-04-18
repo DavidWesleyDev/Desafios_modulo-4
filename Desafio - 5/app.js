916de644b9cf49034f9fa3075f262d9089729378
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-18 20:32:04 -0300

    Desafio 5
    concluído

---
+++ Desafio - 5/app.js
@@ -0,0 +1,6 @@
+let valor1 = Number(prompt('Me fale um numero:'));
+let valor2 = Number(prompt('Me fale outro numero:'));
+let resultado = valor1 + valor2;
+
+console.log(`O valor da soma dos dois valolres é : ${resultado}.`);
+alert(`A soma dos numeros ${valor1} e ${valor2} é : ${resultado}.`);
